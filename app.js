f15924811ca9230c91851b77d07a1b8c506c36c3
jpmitchellpierson <jpmitchellpierson@gmail.com>
2018-01-23 18:53:38 -0800

    toggle about options

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
+// Flexslider jQuery
 $(document).ready(function() {
   $('.flexslider').flexslider({
     animation: "slide"
   });
+
+  let aboutMichael = document.createElement('button');
+  aboutMichael.className = 'option';
+  aboutMichael.id = 'aboutMichael';
+  aboutMichael.innerText = 'Michael Pierson'
+
+  let michaelServices = document.createElement('button');
+  michaelServices.className = 'option';
+  michaelServices.id = 'michaelServices'
+  michaelServices.innerText = 'Our Services';
+
+  $('.about').append(aboutMichael);
+  $('.about').append(michaelServices);
+  $('#aboutMichael').hide();
+  $('#michaelServices').hide();
+
+  $('#about').on('click',() => {
+    $('#aboutMichael').toggle();
+    $('#michaelServices').toggle();
+  });
 });
+
